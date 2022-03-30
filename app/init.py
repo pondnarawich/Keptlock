8ad11f2775ab6f1bd4bbc337be4ff50b1a2ab7d7
pondnarawich <pondnarawich@gmail.com>
2022-03-30 22:06:57 +0700

    record the video via server

--- app/init.py
+++ app/init.py
@@ -1,16 +1,43 @@
 from flask import Flask
 import requests
-
+import os
 app = Flask(__name__)
 
-@app.route('/')
-def send():
-    response = requests.get('http://127.0.0.1:5000')
+path = r"/home/pondnarawich/Documents/GitHub/keptlock/app/init.py"
+assert os.path.isfile(path)
+with open(path, "r") as f:
+    pass
+
+@app.route('/on')
+def on():
+    response = requests.get('http://127.0.0.1:5000/on')
     r = response.json()
     print(r)
+    print('on')
+    return r
 
+@app.route('/off')
+def off():
+    response = requests.get('http://127.0.0.1:5000/off')
+    r = response.json()
+    print(r)
+    return r
+
+@app.route('/start')
+def start():
+    response = requests.get('http://127.0.0.1:5000/start')
+    r = response.json()
+    print(r)
+    return r
+
+@app.route('/stop')
+def stop():
+    response = requests.get('http://127.0.0.1:5000/stop')
+    r = response.json()
+    print(r)
+    return r
 
 if __name__ == '__main__':
     # run() method of Flask class runs the application
     # on the local development server.
-    app.run()
+    app.run(host='127.0.0.1',port=8000)
