b26ee0bbf45a87005de303e1414111b126dd8484
pondnarawich <pondnarawich@gmail.com>
2022-03-31 19:45:55 +0700

    Update main.py

--- app/main.py
+++ app/main.py
@@ -11,36 +11,71 @@ assert os.path.isfile(path)
 with open(path, "r") as f:
     pass
 
+# user api
+
 
 @app.route('/keptlock/user/register', methods=['POST'])
-def register_api():
+def register_user_api():
     r = request.json
     return r
 
 
 @app.route('/keptlock/user/login', methods=['POST'])
-def login_api():
+def login_user_api():
     r = request.json
     print(r)
     return r
 
 
 @app.route('/keptlock/user/logout/<uid>', methods=['POST'])
-def logout_api(uid):
+def logout_user_api(uid):
     print(uid)
     return uid
 
 
 @app.route('/keptlock/user/<uid>', methods=['PUT', 'GET', 'DELETE'])
-def crud_api(uid):
+def rud_user_api(uid):
     print(uid)
     if request.method == 'PUT':
-        print('post',uid)
+        print('put', uid)
     elif request.method == 'GET':
         print('get', uid)
     elif request.method == 'DELETE':
         print('delete', uid)
     return uid
+
+
+# locker api
+
+
+@app.route('/keptlock/locker/<serial>', methods=['POST'])
+def add_locker_api(serial):
+    print(serial)
+    return serial
+
+
+@app.route('/keptlock/locker', methods=['POST'])
+def create_locker_api():
+    r = request.json
+    return r
+
+
+@app.route('/keptlock/locker', methods=['GET'])
+def readall_locker_api():
+    r = request.json
+    return r
+
+
+@app.route('/keptlock/locker/<lid>', methods=['PUT', 'GET', 'DELETE'])
+def rud_locker_api(lid):
+    print(lid)
+    if request.method == 'PUT':
+        print('put', lid)
+    elif request.method == 'GET':
+        print('get', lid)
+    elif request.method == 'DELETE':
+        print('delete', lid)
+    return lid
 
 
 if __name__ == '__main__':
