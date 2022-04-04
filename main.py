71537a14dff57e96e60010edeb8962fda2886367
m_pitsinee <66128768+pitsinee2206@users.noreply.github.com>
2022-04-04 22:08:26 +0700

    finish register user

--- main.py
+++ main.py
@@ -1,31 +1,106 @@
-from flask import Flask, request, render_template, redirect, flash
+from flask import Flask, request, render_template, redirect, flash, jsonify
+from flask_sqlalchemy import SQLAlchemy
 from flask_login import login_user, login_required, current_user, logout_user, LoginManager
+from werkzeug.security import (generate_password_hash, check_password_hash)
+from sqlalchemy.orm import validates
+from datetime import datetime
+import re
 import os
 import babel
 
 template_dir = os.path.abspath('templates')
 static_dir = os.path.abspath('static')
 app = Flask(__name__, template_folder=template_dir, static_folder=static_dir)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
+db = SQLAlchemy(app)
 
 login_manager = LoginManager()
 login_manager.init_app(app)
 login_manager.session_protection = "strong"
 
 
+# CREATE CLASSS FOR DATABASE
+class User(db.Model):
+    id = db.Column(db.String(15), primary_key=True)
+    fname = db.Column(db.String(20))
+    lname = db.Column(db.String(30))
+    email = db.Column(db.String(100), unique=True, nullable=False)
+    mobile = db.Column(db.String(12), unique=True, nullable=False)
+    username = db.Column(db.String(20), unique=True, nullable=False)
+    password = db.Column(db.String(200), nullable=False)
+    created_at = db.Column(db.DateTime, default=datetime.utcnow)
+    updated_at = db.Column(db.DateTime, default=datetime.utcnow)
+    deleted_at = db.Column(db.DateTime, default=datetime.utcnow)
+
+    is_authenticated = False
+
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
+
+
+class Pin(db.Model):
+    id = db.Column(db.String(15), primary_key=True)
+    code = db.Column(db.String(10))
+    oid = db.Column(db.String(30))
+    date_time = db.Column(db.DateTime, default=datetime.utcnow)
+    slot = db.Column(db.String(12))
+    status = db.Column(db.String(15))
+
+
+class Owner(db.Model):
+    id = db.Column(db.String(15), primary_key=True)
+    uid = db.Column(db.String(10))
+    lid = db.Column(db.String(30))
+
+
+class Locker(db.Model):
+    id = db.Column(db.String(15), primary_key=True)
+    name = db.Column(db.String(20))
+    serial = db.Column(db.String(30))
+    size = db.Column(db.Integer)
+    row = db.Column(db.Integer)
+    col = db.Column(db.Integer)
+
+
+class History(db.Model):
+    id = db.Column(db.String(15), primary_key=True)
+    oid = db.Column(db.String(20))
+    date_time = db.Column(db.DateTime, default=datetime.utcnow)
+    slot = db.Column(db.Integer)
+    vid1 = db.Column(db.String(20))
+    vid2 = db.Column(db.String(20))
+
+
 @login_manager.user_loader
 def load_user(user_id):
     # TODO Chane to query data base kong mng either sql or no sql, this to store user info when already login
+    User.query.filter_by(id=user_id).all()
+    print(User)
 
-    # mock up variable, need to be from checking with db
-    from db_struct.user import User
     user_info = User
-    user_info.id = 12345678
-    user_info.name = "gunn"
-    user_info.username = "gnnchya"
-    user_info.email = "62011118@kmitl.ac.th"
-    user_info.password = "gnnchya"
+    print(User.username)
+    print(User.mobile)
+
+    # user_info.id = User.id
+    # user_info.name = User.fname
+    # user_info.username = User.username
+    # user_info.email = user.emai
+    # user_info.password = "gnnchya"
 
     return user_info
+    # mock up variable, need to be from checking with db
+    # from db_struct.user import User
+    # user_info = User
+    # user_info.id = 12345678
+    # user_info.name = "gunn"
+    # user_info.username = "gnnchya"
+    # user_info.email = "62011118@kmitl.ac.th"
+    # user_info.password = "gnnchya"
+
+    # return user_info
 
 
 @app.route("/")
@@ -55,23 +130,33 @@ def register_api():
     password = request.form['password_reg']
 
     # TODO validate username email mobile. Are they unique? if ok then add to db
+    user = User(id='147111', fname=fname, lname=lname, mobile=mobile,
+                email=email, username=username, password=password)
+    user.set_password(password)
+    # user.validate_username(username)
+    # user.validate_email(email)
 
-    # mock up variable, need to be from checking with db
-    uname_unique = True
-    email_unique = True
-    mobile_unique = True
-
-    if uname_unique and email_unique and mobile_unique:
-        return redirect("http://127.0.0.1:8000/keptlock/user/login")
-    elif uname_unique and email_unique and not mobile_unique:
-        flash("This mobile number has been used, try again")
-        return redirect('http://127.0.0.1:8000/keptlock/user/register')
-    elif uname_unique and not email_unique and mobile_unique:
-        flash("This email has been used, try again")
-        return redirect('http://127.0.0.1:8000/keptlock/user/register')
-    elif not uname_unique and email_unique and mobile_unique:
+    c_username = User.query.filter(User.username == username).first()
+    if c_username:
         flash("This username has been used, try again")
         return redirect('http://127.0.0.1:8000/keptlock/user/register')
+
+    if User.query.filter(User.email == email).first():
+        flash("This email has been used, try again")
+        return redirect('http://127.0.0.1:8000/keptlock/user/register')
+
+    if User.query.filter(User.mobile == mobile).first():
+        flash("This mobile number has been used, try again")
+        return redirect('http://127.0.0.1:8000/keptlock/user/register')
+
+    try:
+        User.is_authenticated = True
+        db.session.add(user)
+        db.session.commit()
+        print("successfully added user")
+        return redirect("http://127.0.0.1:8000/keptlock/user/login")
+    except AssertionError as exception_message:
+        return render_template('error.html'), 400
 
 
 @app.route('/keptlock/user/login')
@@ -89,7 +174,8 @@ def login_api():
     password = request.form['password']
 
     from db_struct.user import User
-    user_info_login = User(12345, "gunn", "chai", "62011118@kmitl.ac.th", "0970047016", "gnnchya", "gnnchya", None, None, None)
+    user_info_login = User(12345, "gunn", "chai", "62011118@kmitl.ac.th",
+                           "0970047016", "gnnchya", "gnnchya", None, None, None)
 
     # TODO Check the user and password in the database
     # mock up variable, need to be from checking with db
@@ -190,10 +276,14 @@ def rud_locker_api(lid):
         pin = [pin1, pin2, pin3, pin4]
 
         from db_struct.history import History
-        his1 = History(1432, 1324234, datetime.datetime.now(), 1, "static/vid.pune.mov", "static/vid.pond.mov")
-        his2 = History(3546, 1324234, datetime.datetime.now(), 2, "static/vid.pune.mov", "static/vid.pond.mov")
-        his3 = History(2343, 1324234, datetime.datetime.now(), 3, "static/vid.pune.mov", "static/vid.pond.mov")
-        his4 = History(1643, 1324234, datetime.datetime.now(), 2, "static/vid.pune.mov", "static/vid.pond.mov")
+        his1 = History(1432, 1324234, datetime.datetime.now(), 1,
+                       "static/vid.pune.mov", "static/vid.pond.mov")
+        his2 = History(3546, 1324234, datetime.datetime.now(), 2,
+                       "static/vid.pune.mov", "static/vid.pond.mov")
+        his3 = History(2343, 1324234, datetime.datetime.now(), 3,
+                       "static/vid.pune.mov", "static/vid.pond.mov")
+        his4 = History(1643, 1324234, datetime.datetime.now(), 2,
+                       "static/vid.pune.mov", "static/vid.pond.mov")
 
         history = [his1, his2, his3, his4]
 
@@ -221,6 +311,12 @@ def not_found(e):
 @app.errorhandler(401)
 def unauthorized(e):
     return redirect('http://127.0.0.1:8000/keptlock/user/login')
+
+
+@app.errorhandler(400)
+def signUp_error(e):
+    flash("Unable to Sign Up")
+    return render_template('error.html'), 400
 
 
 @app.template_filter()
