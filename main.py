8862051c2a1881645973ea1ed0c169767b726d38
gnnchya <75212443+gnnchya@users.noreply.github.com>
2022-04-09 03:16:35 +0700

    Done some basic db
    We can now login, register, logout, see all device, add device, see locker, see pin, see history

--- main.py
+++ main.py
@@ -1,7 +1,13 @@
 from flask import Flask, request, render_template, redirect, flash, session, url_for
 from flask_login import login_user, login_required, current_user, logout_user, LoginManager
 import os
-from babel.dates import format_datetime
+# from babel.dates import format_datetime
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
+from werkzeug.security import (generate_password_hash, check_password_hash)
+import uuid
+import random
+import string
 
 template_dir = os.path.abspath('templates')
 static_dir = os.path.abspath('static')
@@ -10,21 +16,135 @@ app = Flask(__name__, template_folder=template_dir, static_folder=static_dir)
 login_manager = LoginManager()
 login_manager.init_app(app)
 login_manager.session_protection = "strong"
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
+db = SQLAlchemy(app)
+cur_pin = set()
 
+
+# CREATE CLASS FOR DATABASE
+class User(db.Model):
+    id = db.Column(db.String(50), primary_key=True)
+    fname = db.Column(db.String(20))
+    lname = db.Column(db.String(30))
+    email = db.Column(db.String(100), unique=True, nullable=False)
+    mobile = db.Column(db.String(12), unique=True, nullable=False)
+    username = db.Column(db.String(20), unique=True, nullable=False)
+    password = db.Column(db.String(500), nullable=False)
+    is_authenticated = db.Column(db.Boolean, default=True)
+    is_active = db.Column(db.Boolean, default=True)
+    is_anonymous = db.Column(db.Boolean, default=False)
+    created_at = db.Column(db.DateTime, default=datetime.utcnow)
+    updated_at = db.Column(db.DateTime, default=None)
+    deleted_at = db.Column(db.DateTime, default=None)
+
+    def get_id(self):
+        return self.id
+
+    def __repr__(self):
+        return '<User %r>' % self.id
+
+
+class Pin(db.Model):
+    id = db.Column(db.String(50), primary_key=True)
+    code = db.Column(db.String(10))
+    uid = db.Column(db.String(50))
+    lid = db.Column(db.String(50), nullable=False)
+    date_start = db.Column(db.DateTime, default=datetime.utcnow)
+    date_end = db.Column(db.DateTime, default=datetime.utcnow)
+    slot = db.Column(db.String(12))
+    status = db.Column(db.String(15), default='unused')
+
+    def __repr__(self):
+        return '<Pin %r>' % self.id
+
+
+class Owner(db.Model):
+    id = db.Column(db.String(50), primary_key=True)
+    uid = db.Column(db.String(50))
+    lid = db.Column(db.String(50))
+
+    def __repr__(self):
+        return '<Owner %r>' % self.id
+
+
+class Locker(db.Model):
+    id = db.Column(db.String(50), primary_key=True)
+    name = db.Column(db.String(20), default="My locker")
+    serial = db.Column(db.String(50))
+    size = db.Column(db.Integer)
+    row = db.Column(db.Integer)
+    col = db.Column(db.Integer)
+
+    def __repr__(self):
+        return '<Locker %r>' % self.id
+
+
+class History(db.Model):
+    id = db.Column(db.String(50), primary_key=True)
+    uid = db.Column(db.String(50))
+    lid = db.Column(db.String(50))
+    date_time = db.Column(db.DateTime, default=datetime.utcnow)
+    slot_id = db.Column(db.Integer)
+    vid_id = db.Column(db.String(50))
+
+    def __repr__(self):
+        return '<History %r>' % self.id
+
+
+class Slot(db.Model):
+    id = db.Column(db.String(50), primary_key=True)
+    lid = db.Column(db.String(30), nullable=False)
+    opened = db.Column(db.Boolean, nullable=False, default=False)
+
+    def __repr__(self):
+        return '<Slot %r>' % self.id
+
+
+class Video(db.Model):
+    id = db.Column(db.String(50), primary_key=True)
+    uid = db.Column(db.String(50))
+    date_time = db.Column(db.DateTime, default=datetime.utcnow)
+    slot_id = db.Column(db.String(50))
+    vid1 = db.Column(db.String(50))
+    vid2 = db.Column(db.String(50))
+
+    def __repr__(self):
+        return '<Video %r>' % self.id
+
+
+def set_password(password):
+    return generate_password_hash(password)
+
+
+def check_password(hashed, password):
+    return check_password_hash(hashed, password)
+
+
+def id_generator(size=6, chars=string.digits):
+    while True:
+        pin = ''.join(random.choice(chars) for x in range(size))
+        if pin not in cur_pin:
+            cur_pin.add(pin)
+            return pin
+
+
+def create_locker(size=3):
+    locker_id = str(uuid.uuid4())
+    serial = str(uuid.uuid4())
+    print(serial)
+    new_locker = Locker(id=locker_id, serial=serial, size=size, row=size, col=1)
+    db.session.add(new_locker)
+    for i in range(size):
+        slot = Slot(id=str(uuid.uuid4()), lid=locker_id)
+        db.session.add(slot)
+    db.session.commit()
+
+
+# create_locker()
 
 @login_manager.user_loader
 def load_user(user_id):
-    # TODO Chane to query data base kong mng either sql or no sql, this to store user info when already login
-
-    # mock up variable, need to be from checking with db
-    from db_struct.user import User
-    user_info = User
-    user_info.id = 12345678
-    user_info.name = "gunn"
-    user_info.username = "gnnchya"
-    user_info.email = "62011118@kmitl.ac.th"
-    user_info.password = "gnnchya"
-
+    user_info = User.query.filter_by(id=user_id).first()
     return user_info
 
 
@@ -54,15 +174,32 @@ def register_api():
     username = request.form['username_reg']
     password = request.form['password_reg']
 
-    # TODO validate username email mobile. Are they unique? if ok then add to db
-
-    # mock up variable, need to be from checking with db
     uname_unique = True
     email_unique = True
     mobile_unique = True
 
+    non_unique = User.query.filter_by(username=username, email=email, mobile=mobile).all()
+    if non_unique is not None:
+        for user in non_unique:
+            if user.username == username:
+                uname_unique = False
+            elif user.email == email:
+                email_unique = False
+            elif user.mobile == mobile:
+                mobile_unique = False
+
     if uname_unique and email_unique and mobile_unique:
-        return redirect("http://127.0.0.1:8000/keptlock/user/login")
+        hashed = set_password(password)
+        uid = uuid.uuid4()
+        new_user = User(id=str(uid), fname=fname, lname=lname, email=email, mobile=mobile, username=username, password=hashed)
+        try:
+            db.session.add(new_user)
+            db.session.commit()
+            return redirect("http://127.0.0.1:8000/keptlock/user/login")
+        except:
+            flash("Something went wrong, please try again")
+            return redirect('http://127.0.0.1:8000/keptlock/user/register')
+
     elif uname_unique and email_unique and not mobile_unique:
         flash("This mobile number has been used, try again")
         return redirect('http://127.0.0.1:8000/keptlock/user/register')
@@ -85,28 +222,27 @@ def login_page():
 def login_api():
     if current_user.is_authenticated:
         return redirect('http://127.0.0.1:8000/keptlock/locker')
+
     username = request.form['username']
     password = request.form['password']
 
-    from db_struct.user import User
-    user_info_login = User(12345678, "gunn", "chai", "62011118@kmitl.ac.th", "0970047016", "gnnchya", "gnnchya", None, None, None)
+    user = False
+    checked_pass = False
 
-    # TODO Check the user and password in the database
-    # mock up variable, need to be from checking with db
-    user = True
-    checked_pass = True
+    check = User.query.filter_by(username=username).first()
+    if check is not None:
+        user = True
+        if check_password(check.password, password):
+            checked_pass = True
 
-    # No username in the database
     if not user:
         flash('This username is not registered')
         return redirect('http://127.0.0.1:8000/keptlock/user/login')
-    # wrong password
     elif user and not checked_pass:
         flash('Password is incorrect, Try again')
         return redirect('http://127.0.0.1:8000/keptlock/user/login')
-    # login success
     else:
-        login_user(user_info_login, remember=True)
+        login_user(check, remember=True)
         return redirect('http://127.0.0.1:8000/keptlock/locker')
 
 
@@ -135,16 +271,13 @@ def crud_api(uid):
 @login_required
 def lockers_page():
     uid = current_user.id
-    # TODO: query all the device based on user id
-
-    from db_struct.locker import Locker
-    # mock up variable, need to be query from db
-    locker1 = Locker(1234, "Pitsinee", "ABCDEFG", 3, 3, 1)
-    locker2 = Locker(1235, "Mind", "AB12345", 3, 3, 1)
-    locker3 = Locker(1236, "Mickey's owner", "1234EFG", 3, 3, 1)
-    locker4 = Locker(1246, "Minnie's owner", "1245nFG", 3, 3, 1)
-
-    lockers = [locker1, locker2, locker3, locker4]
+    own_ids = Owner.query.filter_by(uid=uid).all()
+    lockers = None
+    if own_ids:
+        lids = []
+        for own in own_ids:
+            lids.append(own.lid)
+        lockers = db.session.query(Locker).filter(Locker.id.in_(lids)).all()
 
     return render_template('device.html', username=current_user.username, lockers=lockers)
 
@@ -153,9 +286,19 @@ def lockers_page():
 @login_required
 def add_locker_api():
     serial = request.form['serial']
-    # TODO: query the device according to the serial to check if the serial exists
 
-    serial_exist = True
+    serial_exist = False
+
+    check = Locker.query.filter_by(serial=serial).first()
+    if check is not None:
+        serial_exist = True
+        new_owner = Owner(id=str(uuid.uuid4()), uid=current_user.id, lid=check.id)
+        try:
+            db.session.add(new_owner)
+            db.session.commit()
+        except:
+            flash("Something went wrong, please try again")
+            return redirect("http://127.0.0.1:8000/keptlock/locker#")
 
     if not serial_exist:
         flash("The serial does not exist, please contact admin for further help")
@@ -167,18 +310,18 @@ def add_locker_api():
 @app.route('/keptlock/locker', methods=['POST'])
 @login_required
 def create_locker_api():
-    r = request.json
-    return r
+    return create_locker()
 
 
 @app.route('/keptlock/locker/<lid>', methods=['POST', 'PUT', 'GET', 'DELETE'])
 def rud_locker_api(lid):
-    print(lid)
-    # TODO query to get uid from lid,
+    check_own = Owner.query.filter_by(lid=lid).all()
+    authorized = False
+    for user in check_own:
+        if user.uid == current_user.id:
+            authorized = True
 
-    # mock up data
-    uid = 12345678
-    if uid != current_user.id:
+    if not authorized:
         flash("You trying to access other's locker!")
         return redirect("http://127.0.0.1:8000/keptlock/locker#")
 
@@ -196,41 +339,29 @@ def rud_locker_api(lid):
                 return redirect("http://127.0.0.1:8000/keptlock/locker/unlock/"+pin)
             if key.startswith('change.'):
                 name = request.form['name']
-                print(name)
-                # TODO change name in the db according to the lid
+                locker_info = Locker.query.filter_by(id=lid).first()
+                locker_info.name = name
+                try:
+                    db.session.commit()
+                except:
+                    flash("Something went wrong, please try again")
                 return redirect("http://127.0.0.1:8000/keptlock/locker/"+lid+"#")
 
     if request.method == 'PUT':
         print('put', lid)
     elif request.method == 'GET':
-        # TODO: query history and active pin of the locker from locker id
+        # current_time = datetime.utcnow()
 
-        # mock up data
-        from db_struct.locker import Locker
-        locker = Locker(1234, "Pitsinee", "ABCDEFG", 3, 3, 1)
+        locker = Locker.query.filter_by(id=lid).first()
+        slots = Slot.query.filter_by(lid=lid).all()
+        pin = Pin.query.filter_by(lid=lid, uid=current_user.id, status='unused').all()
+        history = History.query.filter_by(lid=lid, uid=current_user.id).all()
 
-        from db_struct.slot import Slot
-        slot1 = Slot(1, False)
-        slot2 = Slot(1, False)
-        slot3 = Slot(1, True)
+        if not pin:
+            pin = None
+        if not history:
+            history = None
 
-        from db_struct.pin import Pin
-        import datetime
-        pin1 = Pin(1234, 121212, 1324234, datetime.datetime.now(), 2, "valid")
-        pin2 = Pin(3243, 454545, 1324234, datetime.datetime.now(), 1, "valid")
-        pin3 = Pin(2342, 676767, 1324234, datetime.datetime.now(), 2, "valid")
-        pin4 = Pin(5676, 787878, 1324234, datetime.datetime.now(), 3, "valid")
-
-        pin = [pin1, pin2, pin3, pin4]
-
-        from db_struct.history import History
-        his1 = History(1432, 1324234, datetime.datetime.now(), 1, 3456)
-        his2 = History(3546, 1324234, datetime.datetime.now(), 2, 1234)
-        his3 = History(2343, 1324234, datetime.datetime.now(), 3, 2454)
-        his4 = History(1643, 1324234, datetime.datetime.now(), 2, 6437)
-
-        history = [his1, his2, his3, his4]
-        slots = [slot1, slot2, slot3]
         session["lid"] = lid
         return render_template("locker.html", pins=pin, histories=history, locker=locker, slots=slots, username=current_user.username, lid=lid)
 
@@ -248,40 +379,55 @@ def generate_pin_page(lid):
 @app.route('/keptlock/locker/unlock/pin/<lid>', methods=['POST'])
 def generate_pin_api(lid):
     slot = request.form['open']
-    print(slot)
+    code = id_generator()
     if request.form['time'] == "time_range":
         start = request.form['start_time']
         end = request.form['end_time']
         print(start)
         print(end)
-        # TODO do something with DB
+
+        new_pin = User(id=str(uuid.uuid4()), code=code, uid=current_user.id, slot=slot, date_start=start, date_end=end)
+        try:
+            db.session.add(new_pin)
+            db.session.commit()
+        except:
+            flash("Something went wrong, please try again")
+
     elif request.form['time'] == "time_countdown":
         countdown = request.form['countdown']
-        print(countdown)
 
-        # TODO do something with DB
+        current_time = datetime.utcnow()
+        expired_date = current_time + datetime.timedelta(minutes=countdown)
+
+        new_pin = User(id=uuid.uuid4(), code=code, uid=current_user.id, slot=slot, date_end=expired_date)
+        try:
+            db.session.add(new_pin)
+            db.session.commit()
+        except:
+            flash("Something went wrong, please try again")
 
     return redirect("http://127.0.0.1:8000/keptlock/locker/"+lid+"#")
 
 
 @app.route('/keptlock/locker/unlock/<pid>', methods=['PUT', 'GET', 'DELETE'])
 def rud_pin_api(pid):
-    print(pid)
+    lid = session['lid']
+    check_own = Owner.query.filter_by(lid=lid).all()
+    authorized = False
+    for user in check_own:
+        if user.uid == current_user.id:
+            authorized = True
 
-    # TODO query to get uid from lid,
-
-    # mock up data
-    uid = 12345678
-    if uid != current_user.id:
+    if not authorized:
         flash("You trying to access other's locker!")
         return redirect("http://127.0.0.1:8000/keptlock/locker#")
 
-    lid = session['lid']
     if request.method == 'PUT':
         print('put', pid)
     elif request.method == 'GET':
         print('get', pid)
-        # TODO Delete in the database
+        pin_to_delete = Pin.query.get_or_404(pid)
+        db.session.delete(pin_to_delete)
     elif request.method == 'DELETE':
         print('delete', pid)
     return redirect("http://127.0.0.1:8000/keptlock/locker/" + lid + "#")
@@ -303,13 +449,14 @@ def add_video_api():
 
 @app.route('/keptlock/locker/video/<vid>', methods=['PUT', 'GET', 'DELETE'])
 def rud_video_api(vid):
-    print(vid)
     lid = session["lid"]
-    # TODO query to get uid from lid,
+    check_own = Owner.query.filter_by(lid=lid).all()
+    authorized = False
+    for user in check_own:
+        if user.uid == current_user.id:
+            authorized = True
 
-    # mock up data
-    uid = 12345678
-    if uid != current_user.id:
+    if not authorized:
         flash("You trying to access other's locker!")
         return redirect("http://127.0.0.1:8000/keptlock/locker#")
 
@@ -317,13 +464,14 @@ def rud_video_api(vid):
         print('put', vid)
     elif request.method == 'GET':
         # TODO get all the info from the db from vid
+        video = Video.query.filter_by(id=vid).first()
 
-        # mock up data
-        from db_struct.video import Video
-        import datetime
-        vid = Video(3456, 1324234, datetime.datetime.now(), 1, "pond2.mp4", "pune2.mp4")
+        # # mock up data
+        # from db_struct.video import Video
+        # import datetime
+        # vid = Video(3456, 1324234, datetime.datetime.now(), 1, "pond2.mp4", "pune2.mp4")
 
-        return render_template("video.html", username=current_user.username, vid=vid, lid=lid)
+        return render_template("video.html", username=current_user.username, vid=video, lid=lid)
     elif request.method == 'DELETE':
         print('delete', vid)
         return
@@ -360,13 +508,13 @@ def unauthorized(e):
     return redirect('http://127.0.0.1:8000/keptlock/user/login')
 
 
-@app.template_filter()
-def format_datetime(value, form='date'):
-    if form == 'time':
-        form = "HH:mm"
-    elif form == 'date':
-        form = "dd.MM.yy"
-    return format_datetime(value, form)
+# @app.template_filter()
+# def format_datetime(value, form='date'):
+#     if form == 'time':
+#         form = "HH:mm"
+#     elif form == 'date':
+#         form = "dd.MM.yy"
+#     return format_datetime(value, form)
 
 
 if __name__ == '__main__':
