da5efdfcf99c64e78c7106d4e497307b0cdc94e2
pondnarawich <pondnarawich@gmail.com>
2022-04-15 20:14:02 +0700

    -

---
+++ env.py
@@ -0,0 +1,11 @@
+import os
+
+
+# import the environment
+
+
+LOCAL_IP = os.getenv('LOCAL_IP')
+LOCAL_PORT = os.getenv('LOCAL_PORT')
+PI_IP = os.getenv('PI_IP')
+PI_PORT = os.getenv('PI_PORT')
+PI_PASSWORD = os.getenv('PI_PASSWORD')
---
+++ init_test.py
@@ -0,0 +1,11 @@
+import os
+
+
+# create the environment variable for testing
+
+
+os.environ['LOCAL_IP'] = '127.0.0.1'
+os.environ['LOCAL_PORT'] = 5000
+os.environ['PI_IP'] = '127.0.0.1'
+os.environ['PI_PORT'] = '8000'
+os.environ['PI_PASSWORD'] = 'raspberry'
