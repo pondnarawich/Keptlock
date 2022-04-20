d4f24ba0799363dbd7f150976afef64bdf9052b5
gnnchya <75212443+gnnchya@users.noreply.github.com>
2022-04-20 17:06:34 +0700

    update env related

--- env.py
+++ env.py
@@ -5,14 +5,20 @@ from dotenv import load_dotenv
 # import the environment
 
 
-load_dotenv()
+load_dotenv(override=True)
 
 
-A = os.getenv('LOCAL_IP')
+LOCAL_IP = os.getenv('LOCAL_IP')
 LOCAL_PORT = os.getenv('LOCAL_PORT')
 PI_IP = os.getenv('PI_IP')
 PI_PORT = os.getenv('PI_PORT')
 PI_PASSWORD = os.getenv('PI_PASSWORD')
 
 
-print(A)
+print(LOCAL_IP)
+print(LOCAL_PORT)
+print(PI_IP)
+print(PI_PORT)
+print(PI_PASSWORD)
+
+
