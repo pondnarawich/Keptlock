cad08ce83b2046caf88fb5c9d258a396c8ca96e1
pondnarawich <pondnarawich@gmail.com>
2022-04-20 16:37:43 +0700

    .

--- env.py
+++ env.py
@@ -1,11 +1,18 @@
 import os
+from dotenv import load_dotenv
 
 
 # import the environment
 
 
-LOCAL_IP = os.getenv('LOCAL_IP')
+load_dotenv()
+
+
+A = os.getenv('LOCAL_IP')
 LOCAL_PORT = os.getenv('LOCAL_PORT')
 PI_IP = os.getenv('PI_IP')
 PI_PORT = os.getenv('PI_PORT')
 PI_PASSWORD = os.getenv('PI_PASSWORD')
+
+
+print(A)
